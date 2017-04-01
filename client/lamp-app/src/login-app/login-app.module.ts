cf267e2f5dff036ac8e2259decaffa99f988544d
Timoteo <admin@example.com>
2017-04-01 17:49:28 -0300

    setting environment WIP

--- client/lamp-app/src/login-app/login-app.module.ts
+++ client/lamp-app/src/login-app/login-app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { LoginAppComponent } from './login-app.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  declarations: []
+  declarations: [LoginAppComponent],
+  bootstrap: [LoginAppComponent]
 })
 export class LoginAppModule { }
